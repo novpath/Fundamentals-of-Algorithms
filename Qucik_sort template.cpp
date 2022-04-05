937a35f3498f486a60cf34138d2f1f9d47f8f491
novpath <563959624@qq.com>
2022-04-05 22:24:10 +0800

    Update

--- Qucik_sort template.cpp
+++ Qucik_sort template.cpp
@@ -1,13 +1,25 @@
 /*
 Algorithmic Thinking--Divide and Conquer 
 1.Determine the pivot: q[l], q[(l+r)/2], q[random];
-2.Adjust section:The left section less than x, and the right section larger than x; 
+2.Adjust section:The left section less than x, and the right section larger than x; (key step)
 3.Deal with the left section and right section by recursion;
 
 Algorithmic technique
 Firtly, understand the algorithmic thinking.
 secondly, remember the template for quickly complishing.
-Last, repeating coding the template for 3-5 times.
+Last, repeating code the template for 3-5 times.
+
+How to adjust section?
+Method 1:
+1.Create two array--a[], b[];
+2.Traverse every q[l~r] element, if q[i]<=x; x->a[];
+  if q[i]>x; x->b[];("->" means insert)
+3.a[]->q[], b[]->q[]; 
+Method 2:
+1.Two pointers move from the left and right respectively.
+2.The left pointer 'i' move while the q[i] < x;
+  The right pointer 'j' move while the q[j] > x;
+3.Swap q[i], q[j];
 */
 #include<iostream>
 
